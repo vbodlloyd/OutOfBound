b3f63a81ac8f0a1fac48bc0ee940b1ef8c4dddea
vbodlloyd <vbodlloyd@gmail.com>
2014-10-15 22:37:13 +0200

    Level 4 nearly finish. Door key locked fonction added

---
+++ Out Of Bounds/Assets/DoorAction3.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAction3 : MonoBehaviour {
+
+	public  static bool  close = true;
+
+	void OnTriggerEnter2D (Collider2D rigid )
+	{
+		Debug.Log("enter");
+	}
+	
+	void OnTriggerStay2D(Collider2D rigid){
+		if(Input.GetButtonDown("Jump"))
+		{
+			if(close)
+				return;
+			rigid.gameObject.rigidbody2D.velocity = new Vector2 (0f, 0f);
+			PlayerControler.changeState(1);
+			PlayerControler.changeState(1);// 2 times because of the previousState variable in PlayerControler
+			PlayerControler.changeGravity();
+			Application.LoadLevel ("level3");
+			
+		}
+	}
+}
---
+++ Out Of Bounds/Assets/KeyAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyAction : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D rigid )
+	{
+		Debug.Log("enter");
+	}
+	
+	void OnTriggerStay2D(Collider2D rigid){
+		if(Input.GetButtonDown("Jump"))
+		{
+
+			DoorAction3.close = false;
+			Destroy(gameObject);
+			
+		}
+	}
+}
